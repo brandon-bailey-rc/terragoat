85583cadcfc75d033b2929c5899855d31f0fdc87
Brandon Bailey <81580001+brandon-bailey-rc@users.noreply.github.com>
2021-09-13 12:20:14 -0400

    latest

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -28,6 +28,15 @@ resource "aws_s3_bucket_object" "data_object" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
   tags = merge({
     Name        = "${local.resource_prefix.value}-customer-master"
     Environment = local.resource_prefix.value
